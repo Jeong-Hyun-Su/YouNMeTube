6889fddf294494900969b3a35df497712441952d
Jeong-Hyun-Su <mses1572@naver.com>
2019-05-05 18:02:11 +0900

    유튜브 검색

---
+++ Project/You N Me Tube/Tube.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace You_N_Me_Tube
+{
+    partial class Tube
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_Search = new System.Windows.Forms.TextBox();
+            this.bt_SearchOk = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.thumbnailDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.videoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_Search
+            // 
+            this.tb_Search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_Search.Location = new System.Drawing.Point(21, 56);
+            this.tb_Search.Name = "tb_Search";
+            this.tb_Search.Size = new System.Drawing.Size(910, 28);
+            this.tb_Search.TabIndex = 0;
+            // 
+            // bt_SearchOk
+            // 
+            this.bt_SearchOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_SearchOk.Location = new System.Drawing.Point(937, 56);
+            this.bt_SearchOk.Name = "bt_SearchOk";
+            this.bt_SearchOk.Size = new System.Drawing.Size(84, 33);
+            this.bt_SearchOk.TabIndex = 1;
+            this.bt_SearchOk.Text = "OK";
+            this.bt_SearchOk.UseVisualStyleBackColor = true;
+            this.bt_SearchOk.Click += new System.EventHandler(this.bt_SearchOk_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.thumbnailDataGridViewImageColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn,
+            this.urlDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.videoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 523);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // thumbnailDataGridViewImageColumn
+            // 
+            this.thumbnailDataGridViewImageColumn.DataPropertyName = "Thumbnail";
+            this.thumbnailDataGridViewImageColumn.HeaderText = "Thumbnail";
+            this.thumbnailDataGridViewImageColumn.Name = "thumbnailDataGridViewImageColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "Author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "Author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // urlDataGridViewTextBoxColumn
+            // 
+            this.urlDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.urlDataGridViewTextBoxColumn.DataPropertyName = "Url";
+            this.urlDataGridViewTextBoxColumn.HeaderText = "Url";
+            this.urlDataGridViewTextBoxColumn.Name = "urlDataGridViewTextBoxColumn";
+            this.urlDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // videoBindingSource
+            // 
+            this.videoBindingSource.DataSource = typeof(You_N_Me_Tube.Video);
+            // 
+            // Tube
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 644);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bt_SearchOk);
+            this.Controls.Add(this.tb_Search);
+            this.Name = "Tube";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youtube Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Search;
+        private System.Windows.Forms.Button bt_SearchOk;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource videoBindingSource;
+        private System.Windows.Forms.DataGridViewImageColumn thumbnailDataGridViewImageColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Project/You N Me Tube/Tube.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Google.Apis.Services;
+using Google.Apis.Util;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using YoutubeSearch;
+
+namespace You_N_Me_Tube
+{
+    public partial class Tube : Form
+    {
+        public Tube()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_Search.Text = "exo";
+        }
+
+        public void EucKrToUtf8(string hangle)
+        {
+            var eucKrEncoding = Encoding.GetEncoding("euc-kr");
+            var utf8Encoding = Encoding.UTF8;
+
+            string eucKrString = eucKrEncoding.GetString(eucKrEncoding.GetBytes(hangle));
+            var resultOfUtf8Bytes = utf8Encoding.GetBytes(eucKrString);
+
+            string str = utf8Encoding.GetString(resultOfUtf8Bytes);
+            MessageBox.Show(str);
+            // Assert
+            if (utf8Encoding.GetString(resultOfUtf8Bytes) != hangle) throw new Exception();
+        }
+
+        void bt_SearchOk_Click(object sender, EventArgs e)
+        {
+            VideoSearch items = new VideoSearch();
+            List<Video> list = new List<Video>();
+            foreach(var item in items.SearchQuery(tb_Search.Text, 1))
+            {
+                Video video = new Video();
+                
+                byte[] bytes = Encoding.UTF8.GetBytes(item.Title);
+                video.Title = Encoding.UTF8.GetString(bytes);
+
+                video.Author = item.Author;
+                video.Url = item.Url;
+                byte[] imageBytes = new WebClient().DownloadData(item.Thumbnail);
+                using(MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    video.Thumbnail = Image.FromStream(ms);
+                }
+                list.Add(video);
+            }
+            videoBindingSource.DataSource = list;
+        }
+
+    }
+}
